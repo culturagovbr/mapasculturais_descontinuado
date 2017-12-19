0718bdf616e7eb9065c6839c8acb03f65befe1aa
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-12-19 16:12:16 +0000

    entrypoint.sh esperando o postgres ficar pronto

--- config_files/entrypoint.sh
+++ config_files/entrypoint.sh
@@ -7,6 +7,11 @@ chown -R mapas:www-data /srv/mapas/mapasculturais/src/protected/application
 echo "Starting the postgresql daemon"
 service postgresql start
 
+echo "Wait postgresql start"
+until pg_isready 2>/dev/null; do
+  sleep 1
+done
+
 echo "Deploying app"
 su - mapas -c "cd /srv/mapas/mapasculturais/scripts/ && ./deploy.sh"
 
