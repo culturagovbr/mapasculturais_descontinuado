05df4ae76a1ada53df6ae95ee6ff184da64e4bfb
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-01-21 15:03:43 -0200

    Better modal's UI (ref #148)

--- src/protected/application/themes/BaseV1/layouts/parts/modal/field--input-text.php
+++ src/protected/application/themes/BaseV1/layouts/parts/modal/field--input-text.php
@@ -1,5 +1,7 @@
 <?php
 $className = mb_strtolower($new_entity->getEntityTypeLabel());
+$_placeholder = \MapasCulturais\i::esc_attr__('Título do novo') . ' ' . $className;
 ?>
 <?php $this->part("modal/title", ['title' => $definition['label']]); ?>
-<input type='text' name='<?php echo $field ?>' value="<?php echo $new_entity->$field ?>" <?php if($definition['required']) echo 'required' ?>>
+<input type='text' name='<?php echo $field ?>' placeholder="<?php echo $_placeholder; ?>"
+       value="<?php echo $new_entity->$field ?>" <?php if($definition['required']) echo 'required' ?> >
